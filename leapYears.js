f702decd73f49facdef59514c163af9ceb6707fa
Rachel Spencer <rachelspencer688@gmail.com>
2023-11-10 09:02:39 -0500

    solution

--- leapYears.js
+++ leapYears.js
@@ -5,3 +5,26 @@
 // but years divisible by 400 are leap years.
 // Tested years are in range 1600 ≤ year ≤ 4000.
 
+// Note: this was a Codewars Kata
+
+// function isLeapYear(year){
+//     if (year >= 1600 && year <= 4000){
+//         if (year % 400 === 0){
+//             return true
+//         } else if (year % 100 === 0){
+//             return false
+//         } else if (year % 4 === 0){
+//             return true
+//         }
+//         };
+//     return false
+// };
+
+function isLeapYear(year) {
+    if (year >= 1600 && year <= 4000) {
+        if (year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0)) {
+            return true;
+        }
+    }
+    return false;
+};
